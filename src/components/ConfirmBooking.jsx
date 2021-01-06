f2136fa0d9e8b93dc63d9913f456f44edf65133f
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-01-05 19:40:06 -0500

    Create ConfirmBooking.jsx

---
+++ src/components/ConfirmBooking.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useContext, useEffect, useMemo } from "react";
+import { Row, Col } from "react-bootstrap";
+import "../style/BookingForm.css";
+import StarIcon from "@material-ui/icons/Star";
+import AppContext from "../context/app-context";
+import { getTotNights } from "../lib/datesCalc";
+
+function ConfirmBooking() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ConfirmBooking
